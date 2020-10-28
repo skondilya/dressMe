0dd8c48e3b62bf700eb5ceb8ebe61cd4b98f9d95
shreya kondilya <skondilya@gmail.com>
2020-10-28 15:50:17 +0530

    adding the cart file

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import data from './data';
+import dotenv from 'dotenv';
+import config from './config';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import userRoute from './routes/userRoute';
+import productRoute from './routes/productRoute';
+
+dotenv.config();
+
+const mongodbUrl = config.MONGODB_URL;
+mongoose.connect(mongodbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}).catch(error => console.log(error.reason));
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+
+app.listen(8080, () => { console.log("Server at http://localhost:8080") });
